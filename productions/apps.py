0dae7f53bce4f7f62947e717c659fb52ba7499cb
lijiang <xingkongdadao@gmail.com>
2018-06-26 19:10:07 +0700

    Initial commit

---
+++ productions/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProductionsConfig(AppConfig):
+    name = 'productions'
---
+++ productions/models.py
@@ -0,0 +1,30 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from datetime import timezone, datetime
+
+from django.db import models
+
+
+class Question(models.Model):
+    问题 = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.问题
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
