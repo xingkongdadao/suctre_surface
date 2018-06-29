9c9e476e28d123eee6a19d9387c6c6e0f3e60e75
lijiang <xingkongdadao@gmail.com>
2018-06-30 00:25:16 +0700

    将模板按应用归档

--- employee/views.py
+++ employee/views.py
@@ -5,4 +5,4 @@ from employee import models
 
 def employee_information(request):
     employee_all = models.Employee.objects.all()
-    return render(request, "employee_show.html", {'employee_all': employee_all})
+    return render(request, "employee/employee_show.html", {'employee_all': employee_all})
